a2551f38d7343c259347522855a55bbc6dbfdb8e
M0ysez <35322164+M0ysez@users.noreply.github.com>
2018-01-19 23:27:02 -0600

    Add files via upload

---
+++ CNN_training.m
@@ -0,0 +1,86 @@
+%% Convolution Neural Network Training
+clear all
+clc
+addpath D:\Datasets\Mnist
+addpath D:\Datasets\Cifar\cifar-10-batches-mat
+%% ======================================================================
+%Cargar conjunto de entrenamiento y prueba
+
+% [images,labels,~] = load_cifar10('data_batch_1.mat');
+% labels(labels==0) = 10; % Remap 0 to 10
+% 
+% [testImages,testLabels,meta] = load_cifar10('test_batch.mat');
+% testLabels(testLabels==0) = 10; % Remap 0 to 10
+
+imageDim = 28;
+numClasses = 10;  % Number of classes (MNIST images fall into 10 classes)
+
+images = loadMNISTImages('train-images.idx3-ubyte');
+images = reshape(images,imageDim,imageDim,[]);
+images = Modify_Mnist( images );
+labels = loadMNISTLabels('train-labels.idx1-ubyte');
+labels(labels==0) = 10; % Remap 0 to 10
+
+testImages = loadMNISTImages('t10k-images.idx3-ubyte');
+testImages = reshape(testImages,imageDim,imageDim,[]);
+testImages = Modify_Mnist( testImages );
+testLabels = loadMNISTLabels('t10k-labels.idx1-ubyte');
+testLabels(testLabels==0) = 10; % Remap 0 to 10
+
+%% ======================================================================
+% Crear Red neuronal de convolucion
+
+layer(1).name = 'Conv';
+layer(1).channels = size(images,3);
+layer(1).filterDim = 5;
+layer(1).numFilters = 6;
+layer(1).Stride = 1;
+layer(1).Padding = 0;
+
+layer(2).name = 'Activation';
+layer(2).type = 'relu';
+
+layer(3).name = 'Pooling';
+layer(3).type = 'Mean';
+layer(3).Size = 2;
+layer(3).Stride = 2;
+% % 
+layer(4).name = 'Conv';
+layer(4).filterDim = 5;
+layer(4).numFilters = 30;
+layer(4).Stride = 1;
+layer(4).Padding = 0;
+
+layer(5).name = 'Activation';
+layer(5).type = 'relu';
+
+layer(6).name = 'Pooling';
+layer(6).type = 'Mean';
+layer(6).Size = 2;
+layer(6).Stride = 2;
+
+layer(7).name = 'FC';
+layer(7).Hidden = [0];
+layer(7).OutputSize = numClasses;
+
+% Initialize Parameters
+[theta, hiddenSize] = initialize_weights(imageDim, layer, numClasses);
+
+%%=================================================================
+%Training
+options.epochs = 5;
+options.minibatch = 256;
+options.alpha = 0.1;
+options.momentum = .95;
+
+opttheta = minFuncSGD(@(x,y,z) cnn_cost(x,y,z,numClasses,layer, hiddenSize),...
+                      theta,images,labels,options);
+
+%%======================================================================
+% Test
+
+[~,cost,preds]=cnn_cost(opttheta,testImages,testLabels,numClasses,...
+                layer, hiddenSize, true);
+
+acc = sum(preds==testLabels)/length(preds);
+fprintf('Accuracy is %f\n',acc);
---
+++ minFuncSGD.m
@@ -0,0 +1,116 @@
+function [opttheta] = minFuncSGD(funObj, theta, data, labels, ...
+    options)
+
+%%======================================================================
+% Setup
+assert(all(isfield(options,{'epochs','alpha','minibatch'})),...
+    'Some options not defined');
+if ~isfield(options,'momentum')
+    options.momentum = 0.9;
+end;
+epochs = options.epochs;
+alpha = options.alpha;
+minibatch = options.minibatch;
+numSamples = length(labels); 
+mom = 0.5;
+momIncrease = 20;
+
+velocity = zeros(size(theta));
+
+options.test_interval_iterations = 50;
+options.learning_rate_schedule = 'adagrad';
+options.heuristics_learning_rate_schedule = 'power';
+options.numFigures = 1;
+options.test_results_save_file = 'a' ;
+
+test_interval_iterations = options.test_interval_iterations;
+learning_rate_schedule = options.learning_rate_schedule;
+heuristics_learning_rate_schedule = options.heuristics_learning_rate_schedule;
+if strfind(learning_rate_schedule, 'adadec')
+    adadec_averaging_window_size = options.adadec_averaging_window_size;
+    gradient_histories = {};
+    gradient_histories_oldest = 1;
+    running_sum_gradient_histories = zeros(0);
+end
+
+%%======================================================================
+%% SGD loop
+iteration = 0;
+test_results.iterations = zeros(0);
+test_results.accuracies = zeros(0);
+
+sum_gradients = zeros(0);
+learning_rate = alpha;
+
+for e = 1:epochs
+    
+    % randomly permute indices of data for quick minibatch sampling
+    rp = randperm(numSamples);
+    ticEpoch = tic;
+    for s=1 : minibatch : (numSamples - minibatch + 1)
+        tic;
+        iteration = iteration + 1;
+        
+        % increase momentum after momIncrease iterations
+        if iteration == momIncrease
+            mom = options.momentum;
+        end;
+        
+        % get next randomly selected minibatch
+        mb_data = data(:, :, :,rp(s : min(s + minibatch - 1, end)));
+        mb_labels = labels(rp(s : min(s + minibatch - 1, end)));
+        
+        % evaluate the objective function on the next minibatch
+        [cost, grad] = funObj(theta, mb_data, mb_labels);
+        
+        switch heuristics_learning_rate_schedule
+            case 'power'
+                alpha = alpha / (1 + iteration / 1e4);
+            case 'exponential'
+                alpha = alpha * pow(10,  -iteration / 1e4);
+        end
+        if strfind(learning_rate_schedule, 'adagrad')
+            if isempty(sum_gradients)
+                sum_gradients = grad .^ 2;
+            else
+                sum_gradients = sum_gradients + grad .^ 2;
+            end
+            learning_rate = alpha ./ sqrt(1 + sum_gradients);
+        end
+        if strfind(learning_rate_schedule, 'adadec')
+            oldest = mod(gradient_histories_oldest - 1, adadec_averaging_window_size) + 1;
+            if gradient_histories_oldest > adadec_averaging_window_size
+                running_sum_gradient_histories = running_sum_gradient_histories - gradient_histories{oldest};
+            end
+            gradient_histories{oldest} = grad .^ 2;
+            gradient_histories_oldest = gradient_histories_oldest + 1;
+            if isempty(running_sum_gradient_histories)
+                running_sum_gradient_histories = gradient_histories{oldest};
+                sum_gradients = running_sum_gradient_histories;
+            else
+                running_sum_gradient_histories = running_sum_gradient_histories + gradient_histories{oldest};
+                sum_gradients = options.adadec_forgetting_factor * ...
+                    sum_gradients + running_sum_gradient_histories;
+            end
+            learning_rate = alpha ./ sqrt(1 + sum_gradients);
+        end
+        velocity = mom * velocity + learning_rate .* grad;
+        theta = theta - velocity;
+        
+        fprintf('Epoch %d: Cost on iteration %d is %f\n', e, iteration, cost);
+        toc;
+
+    end;
+    fprintf('Epoch %d\n', e);
+    toc(ticEpoch);
+    if strcmp(heuristics_learning_rate_schedule, 'half_per_epoch')
+        alpha = alpha / 2.0;
+    end
+end;
+
+opttheta = theta;
+
+save(options.test_results_save_file, 'test_results');
+
+end
+
